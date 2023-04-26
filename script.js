3fd4abd5364a29670461a04e9dfeb61616d93643
Roketskyi Roman <91413976+Roketskyi@users.noreply.github.com>
2023-04-26 14:15:22 +0300

    update

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 const fs = require('fs');
 
 const searchTree = (node, searchValue, path = []) => {
-  if (node && node.Node && node.Node.Label === searchValue) {
-    return [...path, node.Node.Label];
-  } else if (node && node.Children) {
-    for (let i = 0; i < node.Children.length; i++) {
-      const result = searchTree(node.Children[i], searchValue, [...path, node.Node.Label]);
+  const regex = new RegExp(`^${formattedSearchValue.replace(/\*/g, '.*')}$`, 'i');
 
-      if (result) {
-        return result;
-      }
+  if (node && node.Node && regex.test(node.Node.Label)) {
+    const labelName = node.Node.Label.Name ? node.Node.Label.Name : node.Node.Label;
+    results.push([...path, labelName]);
+  }
+
+  if (node && node.Children) {
+    for (let i = 0; i < node.Children.length; i++) {
+      searchTree(node.Children[i], [...path, node.Node.Label]);
     }
   }
-  return null;
 };
 
 const isValidValue = (node, searchValue) => {
-  if (node.Node && node.Node.Label === searchValue) {
+  const regex = new RegExp(`^${searchValue.replace(/\*/g, '.*')}$`, 'i');
+
+  if (node.Node && regex.test(node.Node.Label)) {
     return true;
   } else if (node.Children) {
     for (let i = 0; i < node.Children.length; i++) {
@@ -44,32 +46,41 @@ const tree = JSON.parse(fileContents);
 
 const searchValue = process.argv[2];
 
-if (!isValidValue(tree, searchValue)) {
-  console.log(`${searchValue} is not a valid language name.`);
+if (!searchValue) {
+  console.log('Please enter a search value. Example: "node script.js Ukr*" or "node script.js Ukrainian"');
 } else {
-  const rootNode = tree.Root;
-  const results = [];
+  const formattedSearchValue = searchValue.charAt(0).toUpperCase() + searchValue.slice(1).toLowerCase();
 
-  const searchInNode = (node, path) => {
-    if (node && node.Node && node.Node.Label === searchValue) {
-      results.push([...path, node.Node.Label]);
-    }
-
-    if (node && node.Children) {
-      for (let i = 0; i < node.Children.length; i++) {
-        searchInNode(node.Children[i], [...path, node.Node.Label]);
-      }
-    }
-  };
-
-  searchInNode(rootNode, []);
-
-  if (results.length === 0) {
-    console.log(`${searchValue} was not found in the tree.`);
+  if (!isValidValue(tree, formattedSearchValue)) {
+    console.log(`${formattedSearchValue} is not a valid language name.`);
   } else {
-    console.log('Results:');
-    for (const result of results) {
-      console.log(result.join(' -> '));
+    const rootNode = tree.Root;
+    const results = [];
+
+    const searchInNode = (node, path) => {
+      const regex = new RegExp(`^${formattedSearchValue.replace(/\*/g, '.*')}$`, 'i');
+    
+      if (node && node.Node && regex.test(node.Node.Label)) {
+        const labelName = node.Node.Language ? node.Node.Language.Name : node.Node.Label;
+        results.push([...path, labelName]);
+      }
+    
+      if (node && node.Children) {
+        for (let i = 0; i < node.Children.length; i++) {
+          searchInNode(node.Children[i], [...path, node.Node.Label]);
+        }
+      }
+    };    
+
+    searchInNode(rootNode, []);
+
+    if (results.length === 0) {
+      console.log(`${formattedSearchValue} was not found in the tree.`);
+    } else {
+      console.log('Results:');
+      for (const result of results) {
+        console.log(result.join(' -> '));
+      }
     }
   }
 }
